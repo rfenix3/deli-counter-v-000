a393275bc7c8bfb659de93af6082fbcc8ab22778
Richard Fenix <richard.fenix@gmail.com>
2018-05-27 00:17:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+#katz_deli = []
+katz_deli = ["Ada", "Grace", "Kent"]
+
+def line(customers)
+  if customers.length == 0 
+    puts "The line is currently empty."
+  else
+    lineStatus = customers.each_with_index.map{|customer, index| "#{index+1}. #{customer}"}
+    puts "The line is currently: #{lineStatus.join(" ")}"
+  end
+end
+
+puts line(katz_deli)
